5969840d7064c5ed2769fce03a4f3704ffdc1e9d
younahjeon <hiyuna1993@gmail.com>
2021-11-30 12:50:19 -0500

    changed texts/descriptions

---
+++ .ipynb_checkpoints/default_new-checkpoint.js
@@ -0,0 +1,92 @@
+{
+"CAMERAS": {
+  "camera00": {
+   "type": "",
+   "fieldOfView": "",
+   "near": 0.1,
+   "far": 2000,
+   "position": {
+    "x": [
+    ],
+    "y": [
+    ],
+    "z": [
+    ]
+   },
+   "targetTHREEJS": {
+    "x": [
+    ],
+    "y": [
+    ],
+    "z": [
+    ]
+   },
+   "visible": [
+    1
+   ]
+  }
+ },
+ "LIGHTS": {
+  "light00": {
+   "type": "DirectionalLight",
+   "color": "0xffffff",
+   "intensity": [5],
+   "position": {
+    "x": [0
+    ],
+    "y": [1
+    ],
+    "z": [1
+    ]
+   },
+   "visible": [
+    0
+   ]},
+   "light01": {
+    "type": "AmbientLight",
+    "color": "0xffffff",
+    "intensity": [5],
+    "position": {
+     "x": [0
+     ],
+     "y": [1
+     ],
+     "z": [1
+     ]
+    },
+    "visible": [
+     1
+    ]
+  }
+ },
+ "OBJECTS": {
+ },
+ "IMAGES": {
+  "imagebag": "",
+  "imageidx": [],
+  "sizeTHREEJS": []
+ },
+ 
+ "OBJECTFILTERS": {
+   "blur" : [],
+   "brightness" : [],
+   "contrast" : [],
+   "grayscale" : [],
+   "huerotate" : [], 
+   "invert": [],
+   "opacity" : [],
+   "saturate": [],
+   "sepia": []
+ }, 
+ "IMAGEFILTERS": {
+  "blur" : [],
+  "brightness" : [],
+  "contrast" : [],
+  "grayscale" : [],
+  "huerotate" : [], 
+  "invert": [],
+  "opacity" : [],
+  "saturate": [],
+  "sepia": []
+ }
+}
